7481c33c6a65dae187a15459043afbadbf6ad7ac
Henrique-ikeda <riqueikeda2004@gmail.com>
2025-12-08 18:56:45 -0300

    ex7

---
+++ ex7/main.py
@@ -0,0 +1,21 @@
+
+estoque = {
+
+    "Caderno universitário": 50,
+
+    "Caneta azul": 120,
+
+    "Borracha branca": 30
+
+}
+
+
+item = input("Nome do produto a ser atualizado:")
+quantidade = input("Nova quantidade:")
+
+if item in estoque:
+    estoque[item] = quantidade
+    print("Quantidade atualizada com sucesso!!")
+    print(estoque)
+else:
+    print("Produto não encontrado no estoque.")
