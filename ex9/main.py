0d6c51f2398eb449d157b16a2ba7f54e896692cc
Henrique-ikeda <riqueikeda2004@gmail.com>
2025-12-09 14:37:42 -0300

    ex9

---
+++ ex9/main.py
@@ -0,0 +1,16 @@
+
+participantes = {
+
+    "Workshop 1": {"Alice", "Bruno", "Carla", "Diego"},
+
+    "Workshop 2": {"Fernanda", "Gustavo", "Helena"}
+
+}
+
+nome = input("Nome do participante a ser removido:")
+
+for workshop, nomes in participantes.items():
+    nomes.discard(nome)
+print("Lista atualizada de participantes:")
+for workshop, nomes in participantes.items():
+    print(f"{workshop}:{nomes}")
